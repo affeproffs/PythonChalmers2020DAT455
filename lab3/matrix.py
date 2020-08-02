d7bb6d80c11b7c44f53cb2148097c92886409125
Alfred Stenborg <alfst109@student.liu.se>
2020-08-02 17:19:20 +0200

    We one-liners now

--- lab3/matrix.py
+++ lab3/matrix.py
@@ -1,6 +1,5 @@
 def transpose(m):
-    if not m: return m
-    return [[row[i] for row in m] for i in range(len(m[0]))]
+    return m if not m else [[row[i] for row in m] for i in range(len(m[0]))]
 
 def powers(lst, p1, p2):
     return [[v ** p for p in range(p1, p2 + 1)] for v in lst]
@@ -9,7 +8,6 @@ def sumLists(l1, l2):
     return sum([l1[i] * l2[i] for i in range(len(l1))])
 
 def matmul(m1, m2):
-    if not m1: return m1
     return [[sumLists(m1[r], [l[c] for l in m2]) for c in range(len(m2[0]))] for r in range(len(m1))]
 
 
