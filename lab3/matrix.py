165000921d027a6c7c059095404272566bc57d11
Alfred Stenborg <alfst109@student.liu.se>
2020-08-02 17:36:00 +0200

    Lab3 21/21

--- lab3/matrix.py
+++ lab3/matrix.py
@@ -10,8 +10,9 @@ def sumLists(l1, l2):
 def matmul(m1, m2):
     return [[sumLists(m1[r], [l[c] for l in m2]) for c in range(len(m2[0]))] for r in range(len(m1))]
 
+def invert(m):
+    det = m[0][0] * m[1][1] - m[0][1] * m[1][0]
+    return [[m[1][1] / det, -m[0][1] / det], [-m[1][0] / det, m[0][0] / det]]
 
-
-
-        
-    
+def loadtxt(fName):
+    return [list(map(float, l.strip().split("\t"))) for l in open(fName)]
