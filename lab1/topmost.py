fec6ce62f2a1c5bcb8e1623329b742c980630154
Alfred Stenborg <alfst109@student.liu.se>
2020-08-13 13:53:57 +0200

    Lab1 resubmission. Web adapted

--- lab1/topmost.py
+++ lab1/topmost.py
@@ -1,9 +1,15 @@
 from wordfreq import *
 from sys import argv
+import urllib.request
 
 def main():
+    if("http://" in argv[2][:7] or "https://" in argv[2][:8]):
+        lines = urllib.request.urlopen(argv[2]).read().decode("utf8").splitlines()
+    else:
+        lines = open(argv[2]).readlines() # Scopes in python? OMEGALUL
+
+    tokens = tokenize(["".join(w.strip() + " " for w in lines)])        
     stopWords = [w.strip() for w in open(argv[1]).readlines()]
-    tokens = tokenize(["".join(w.strip() + " " for w in open(argv[2]).readlines())])
     printTopMost(countWords(tokens, stopWords), int(argv[3]))
 
 main()
--- lab1/wordfreq.py
+++ lab1/wordfreq.py
@@ -2,7 +2,7 @@ import re
 
 def tokenize(lines):
     if not lines: return lines
-    digits = re.compile('(\d+|[^\w\s])').sub(r' \1 ', lines[0].strip().lower())
+    digits = re.compile('(\d+|[^a-zA-Z\s])').sub(r' \1 ', lines[0].strip().lower())
     spaces = re.compile('(\s+)').sub(' ', digits)
     return list(filter(lambda w: w != '', spaces.split(" ")))
 
@@ -10,4 +10,4 @@ def countWords(words, stopWords): return {w : words.count(w) for w in words if w
 
 def printTopMost(frequencies,n):
     for wf in list(sorted(frequencies.items(), key = lambda i: -i[1]))[:n]:
-        print(wf[0].ljust(19), str(wf[1]).rjust(5)) # Why 19 & not 20 (???)
+        print(wf[0].ljust(20) + str(wf[1]).rjust(5))
